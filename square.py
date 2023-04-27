7b899f59f6eda32e2e07758f340866806421b252
madhantrt <132008249+madhantrt@users.noreply.github.com>
2023-04-28 00:18:34 +0530

    Add files via upload

---
+++ square.py
@@ -0,0 +1,19 @@
+import time
+from flyt_python import api
+drone = api.navigation(timeout=120000)
+
+time.sleep(3)
+
+print 'taking off'
+drone.take_off(5.0)
+
+print 'starting mission'
+drone.position_set(6.5,0,0,relative=True)
+drone.position_set(0,6.5,0,relative=True)
+drone.position_set(-6.5,0,0,relative=True)
+drone.position_set(0,-6.5,0,relative=True)
+
+print 'Landing'
+drone.land(async=False)
+
+drone.disconnect()
